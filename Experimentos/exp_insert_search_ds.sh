786895cbe2cfef3fa8a444e3cde45573d6407be4
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2026-01-27 10:04:55 -0300

    correções

--- Experimentos/exp_insert_search_ds.sh
+++ Experimentos/exp_insert_search_ds.sh
@@ -8,28 +8,29 @@
 #              coletando metricas de tempo, altura da árvore e afins
 
 ## VARIAVEIS E CONFIGURAÇÕES
-OUTPUT_DIR="$HOME/Output"
-NUM_INSTANCIAS=10000
 TS=$(date +"%Y-%m-%d_%H-%M")
+OUTPUT_DIR="$HOME/Output/$TS"
+NUM_INSTANCIAS=500000 # Para cada stream sintetica
 NUMA_NODE=0
-NUMA_MEM_GB=$(numactl --hardware | awk "/node $NUMA_NODE size/ {print int(\$4/1024)}")
+# NUMA_MEM_GB=$(numactl --hardware | awk "/node $NUMA_NODE size/ {print int(\$4/1024)}")
 # MAX_HEAP_GB=$(( NUMA_MEM_GB * 85 / 100 ))
 
-# echo "NUMA node $NUMA_NODE com ${NUMA_MEM_GB}GB (heap máx: ${MAX_HEAP_GB}GB)"
+echo "NUMA node $NUMA_NODE com ${NUMA_MEM_GB}GB (heap máx: ${MAX_HEAP_GB}GB)"
 cd "$HOME"
 mkdir -p "$OUTPUT_DIR"
 
 # Clonar o moa e colocar na branch dos experimentos
-# if [ ! -d "moa" ]; then
-#     echo ">> Clonando MOA..."
-#     git clone https://github.com/pedrobiqua/moa.git
-# else
-#     echo ">> MOA já existente"
-# fi
+if [ ! -d "moa" ]; then
+    echo ">> Clonando MOA..."
+    git clone https://github.com/pedrobiqua/moa.git
+else
+    echo ">> MOA já existente"
+fi
 
-cd /home/pedro/projects/moa
-# git fetch
-# git checkout exp/experiments-balancing
+# cd /home/pedro/projects/moa
+cd moa
+git fetch
+git checkout exp/experiments-balancing
 
 
 ## Compilar
@@ -45,7 +46,7 @@ echo ">> Iniciando experimentos..."
 ### Usar a classe com os parametros certos e executar
 ### No java ele já coloca o nome certinho, é só executar, vou copiar para montar
 
-OUTPUT_MAIN="$OUTPUT_DIR/insert_search_${TS}.csv"
+OUTPUT_MAIN="$OUTPUT_DIR/temp.csv" # Coloco assim, mas no java eu troco o nome
 
 ## -e 0 é o experimento de insert e search
 ## -e 1 é o experimento da janela deslizante
